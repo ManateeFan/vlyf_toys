992bf9fbe8cac77db155509e8296002fecb2c3e0
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-11-04 20:30:11 +0800

    NewCalendar

--- NewCalendar/mainwindow.cpp
+++ NewCalendar/mainwindow.cpp
@@ -6,6 +6,9 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QSpinBox>
+#include <QTextTableFormat>
+#include <QTextTable>
+#include <QTextFrame>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -35,6 +38,9 @@ MainWindow::MainWindow(QWidget *parent) :
     QSpinBox *sizeSpinBox = new QSpinBox;
     sizeSpinBox->setValue(14);  //默认字体大小
     sizeSpinBox->setRange(1,64);//字体设置范围
+
+    editor = new QTextBrowser;
+
     //插入Calendar
     insertCalendar();
 
@@ -55,6 +61,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QVBoxLayout *vboxLayout = new QVBoxLayout;
     vboxLayout->addLayout(hboxLayout);
+    vboxLayout->addWidget(editor,1);
     centralWidget->setLayout(vboxLayout);
 
     setCentralWidget(centralWidget);
@@ -69,6 +76,80 @@ MainWindow::~MainWindow()
 }
 void MainWindow::insertCalendar()
 {
+    editor->clear();
+    QTextCursor cursor = editor->textCursor();
+    cursor.beginEditBlock();
+
+    QDate date(selectDate.year(),selectDate.month(),1);
+
+    QTextTableFormat tableFormat;
+    tableFormat.setAlignment(Qt::AlignHCenter); //对齐方式
+    tableFormat.setBackground(QColor(Qt::green));
+    tableFormat.setCellSpacing(4);  //外边距
+    tableFormat.setCellPadding(2);  //内边距
+
+    QVector<QTextLength> constraints;
+    constraints<< QTextLength(QTextLength::PercentageLength,14)
+               << QTextLength(QTextLength::PercentageLength, 14)
+               << QTextLength(QTextLength::PercentageLength, 14)
+               << QTextLength(QTextLength::PercentageLength, 14)
+               << QTextLength(QTextLength::PercentageLength, 14)
+               << QTextLength(QTextLength::PercentageLength, 14)
+               << QTextLength(QTextLength::PercentageLength, 14);
+    tableFormat.setColumnWidthConstraints(constraints);
+
+
+    QTextTable *table = cursor.insertTable(1,7,tableFormat);
+
+    QTextFrame *frame = cursor.currentFrame();
+
+    QTextFrameFormat frameFormat = frame->frameFormat();
+    frameFormat.setBorder(1);
+    frame->setFrameFormat(frameFormat);
+
+
+    QTextCharFormat format = cursor.charFormat();
+    format.setFontPointSize(fontSize);
+    QTextCharFormat boldFormat = format;
+
+    boldFormat.setFontWeight(QFont::Bold);
+
+    QTextCharFormat highlightedFormat = boldFormat;
+    highlightedFormat.setBackground(Qt::yellow);
+
+    for(int weekDay = 1; weekDay<=7;weekDay++)
+    {
+        QTextTableCell cell = table->cellAt(0, weekDay-1);        QTextCursor cellCursor = cell.firstCursorPosition();
+        cellCursor.insertText(QString("%1").arg(QDate::longDayName(weekDay)),boldFormat);
+
+    }
+
+
+    table->insertRows(table->rows(),1);
+
+    while(date.month()==selectDate.month())
+    {
+        int weekDay = date.dayOfWeek();
+        QTextTableCell cell = table->cellAt(table->rows()-1,weekDay-1);
+        QTextCursor cellCursor = cell.firstCursorPosition();
+
+        if(date == QDate::currentDate())
+            cellCursor.insertText(QString("%1").arg(date.day()),highlightedFormat);
+        else
+            cellCursor.insertText(QString("%1").arg(date.day()),format);
+        date = date.addDays(1);
+        if(weekDay==7 && date.month()==selectDate.month())
+            table->insertRows(table->rows(),1);
+
+    }
+    cursor.endEditBlock();
+
+
+    setWindowTitle(tr("Calendar for %1 %2"
+        ).arg(QDate::longMonthName(selectDate.month())
+        ).arg(selectDate.year()));
+
+
 
 }
 void MainWindow::setFontSize(const int &size)
