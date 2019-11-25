9ad31e9add863e800ab598d05ef7dee75264da72
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-11-25 17:01:15 +0800

    Update

--- VlyfNotepad/VlyfNotePad.pro
+++ VlyfNotepad/VlyfNotePad.pro
@@ -26,13 +26,16 @@ CONFIG += c++11
 
 SOURCES += \
         main.cpp \
-        mainwindow.cpp
+        mainwindow.cpp \
+    finddialog.cpp
 
 HEADERS += \
-        mainwindow.h
+        mainwindow.h \
+    finddialog.h
 
 FORMS += \
-        mainwindow.ui
+        mainwindow.ui \
+    finddialog.ui
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
---
+++ build-VlyfNotePad-Desktop_Qt_5_9_8_MinGW_32bit-Debug/ui_finddialog.h
@@ -0,0 +1,58 @@
+/********************************************************************************
+** Form generated from reading UI file 'finddialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.8
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_FINDDIALOG_H
+#define UI_FINDDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QHeaderView>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_FindDialog
+{
+public:
+    QDialogButtonBox *buttonBox;
+
+    void setupUi(QDialog *FindDialog)
+    {
+        if (FindDialog->objectName().isEmpty())
+            FindDialog->setObjectName(QStringLiteral("FindDialog"));
+        FindDialog->resize(391, 136);
+        buttonBox = new QDialogButtonBox(FindDialog);
+        buttonBox->setObjectName(QStringLiteral("buttonBox"));
+        buttonBox->setGeometry(QRect(290, 20, 81, 241));
+        buttonBox->setOrientation(Qt::Vertical);
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+
+        retranslateUi(FindDialog);
+        QObject::connect(buttonBox, SIGNAL(accepted()), FindDialog, SLOT(accept()));
+        QObject::connect(buttonBox, SIGNAL(rejected()), FindDialog, SLOT(reject()));
+
+        QMetaObject::connectSlotsByName(FindDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *FindDialog)
+    {
+        FindDialog->setWindowTitle(QApplication::translate("FindDialog", "Dialog", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class FindDialog: public Ui_FindDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_FINDDIALOG_H
